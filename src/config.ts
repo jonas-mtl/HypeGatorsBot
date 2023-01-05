3df4a2edae12ec6946fcb0321292347894a119ac
Jonas <98329976+JonasThierbach@users.noreply.github.com>
2023-01-05 14:09:45 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,41 +1,41 @@
 import { Config as IConfig } from './Structures/Interfaces/index.js';
 
 const Config: IConfig = {
-    TOKEN: 'MTAyMTg1ODEwOTEyMTExMDExOA.G7UQ9p.BUbPJYtwoKuon5kKBBEVneYGqeBSGb0m9y7l7k',
+    TOKEN: '',
     Twitter: {
         username: 'HypeGators',
-        id: '1541540551966834688',
+        id: '',
     },
     Database: {
         MongoDB:
-            'mongodb+srv://discordbot:PVJ4hIORBVWWsFrZ@cluster0.z63o9tw.mongodb.net/?retryWrites=true&w=majority',
+            'mongodb-url',
     },
     DevGuilds: [
         {
             name: 'HYPE GATORS',
-            id: '1011006517283143740',
-            modChannel: '1011007190447960094',
+            id: '',
+            modChannel: '',
         },
     ],
     OwnerIds: [
         {
             name: 'Jonas',
-            id: '783252406753689601',
+            id: '',
         },
     ],
     AdminIds: [
         {
             name: 'Jonas',
-            id: '783252406753689601',
+            id: '',
         },
     ],
     APIs: [
         {
             name: 'TwitterAPI',
             apiKeys: [
-                'Ah7uEkLFwCg82yFfFQ3dHbJRf', //API KEY
-                'o1qgZOBwSajnknT98nlQANyqSpwd8jl66qfrOaL2YBWvnmcBgG', // API SECRET
-                'AAAAAAAAAAAAAAAAAAAAACVGiAEAAAAAMMhOqs%2FKQIJEh6csrt1Iq1uzrWc%3DGYfrfmJiyc2HCHk2ZKOGiRIBr5AlsGr0GZkRTm1tZZwlYQ1pxj', //Bearer Token
+                '', //API KEY
+                '', // API SECRET
+                '', //Bearer Token
             ],
         },
     ],
